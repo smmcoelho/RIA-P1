3a66cf2e040bfab2c939fab1b0b99660cc45bd36
smmcoelho <77806811+smmcoelho@users.noreply.github.com>
2025-12-13 09:02:10 +0000

    Create removerDuplicados.py

---
+++ removerDuplicados.py
@@ -0,0 +1,11 @@
+
+def removerDuplicados(l: list) -> list:
+
+
+if __name__ == '__main__':
+    l = [1, 1, 2, 3, 2, 4, 3]
+    new_l = removerDuplicados(l)
+
+    expected_list = [1, 2, 3, 4]
+
+    assert(new_l == expected_list)
